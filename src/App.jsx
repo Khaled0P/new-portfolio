1e330c5c5c0dd3c9faca93581b23fa5daf8fc8d5
Khaled <aa.khaled11@gmail.com>
2025-08-04 02:01:51 +0300

    add projects section with mock projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/Hero"
+import ShowcaseSection from "./sections/ShowcaseSection"
 
 const App = () => {
   return (
     <main className="App">
     <Hero />
+    <ShowcaseSection />
   </main>
   )
 }

20546c225966350eb1909e457c42fe714900cd85
Khaled <aa.khaled11@gmail.com>
2025-08-06 23:07:59 +0300

    update vite config for optimization

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    target: 'esnext', 
+    minify: 'esbuild',
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+          drei: ['@react-three/drei'],
+          postprocessing: ['postprocessing'],
+        },
+      },
+    },
+  },
+});
+
